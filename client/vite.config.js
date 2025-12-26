317b71e8613f0b3b2137cfc0fe1946a8134baafc
Krishna Shukla <backend.9developer@gmail.com>
2025-12-27 03:00:10 +0530

    vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,31 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss(),],
+// })
+
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // allow access from network
+  },
+  preview: {
+    host: 'learn-step-farebuzzertravel-frontend.skxdwz.easypanel.host',
+    port: 5173, // or any allowed port
+    strictPort: true,
+    allowedHosts: [
+      'learn-step-farebuzzertravel-frontend.skxdwz.easypanel.host'
+    ]
+  }
 })
